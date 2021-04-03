60a2832c0e693a4fb2dccb3f5ae7fd721344c784
Minoplhy <kraneberru@disroot.org>
2021-04-03 21:56:37 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,6 +23,6 @@ apt-get -y install git
 
 git clone https://github.com/minoplhy/alive-test/
 rm alive-test/index.html
-git add index.html
+git remote add origin https://github.com/minoplhy/alive-test/index.html
 git commit -m "Autorun"
-git push alive-test main
+git push origin main
